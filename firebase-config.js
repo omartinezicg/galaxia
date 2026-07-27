8252b138ce73acaea410f9ae65ae53c02c0009f4
Oscar Martinez <omartinez@icg.com.gt>
2026-07-27 15:43:49 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+/* =====================================================================
+   CONFIGURACIÓN DE FIREBASE  (proyecto: acuario-fb2f4)
+   ---------------------------------------------------------------------
+   IMPORTANTE sobre databaseURL:
+   Este valor solo existe cuando creas la Realtime Database.
+   Ve a  Build -> Realtime Database -> Crear base de datos (modo de prueba)
+   y confirma que la URL que te muestra la consola sea EXACTAMENTE la de
+   abajo. Si tu base quedó en otra región (Europa, etc.), la URL será
+   parecida a:
+     https://acuario-fb2f4-default-rtdb.europe-west1.firebasedatabase.app
+   En ese caso, reemplaza el databaseURL de abajo por el que te muestre
+   la consola.
+   ===================================================================== */
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCZwZn0JqpSbPdwK1hlwqXgLC9uF2gRF5A",
+  authDomain: "acuario-fb2f4.firebaseapp.com",
+  databaseURL: "https://acuario-fb2f4-default-rtdb.firebaseio.com",
+  projectId: "acuario-fb2f4",
+  storageBucket: "acuario-fb2f4.firebasestorage.app",
+  messagingSenderId: "364956680058",
+  appId: "1:364956680058:web:8460311b93a2216d32e0c3"
+};
